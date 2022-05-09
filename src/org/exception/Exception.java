ddb974f7a81a5d833fe6edf31be87c9785f4f62a
srimathi-gobu <srimathiakilan22@gmail.com>
2022-05-10 00:38:42 +0530

    Build is ready

---
+++ src/org/exception/Exception.java
@@ -0,0 +1,21 @@
+package org.exception;
+
+public class Exception  {
+	public static void main(String[] args) {
+		
+	
+			int [] ar=new int[5];
+			ar[4]=5;
+			try {
+				System.out.println(ar[5]);
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("handle the exception");
+			}
+		
+
+			
+		}}
+	
+
+
